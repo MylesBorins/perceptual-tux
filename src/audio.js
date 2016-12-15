284d0c1cf07bb0e8dcbec1b3c5b0d6ccc2c21d2d
Myles Borins <myles.borins@gmail.com>
2016-12-15 04:28:04 -0500

    Basic Mobile Support

--- src/audio.js
+++ src/audio.js
@@ -1,19 +1,4 @@
-// panner taken from https://developer.mozilla.org/en-US/docs/Web/API/PannerNode
-
-var context = new (window.AudioContext || window.webkitAudioContext)();
-var panner = context.createPanner();
-panner.panningModel = 'HRTF';
-panner.distanceModel = 'inverse';
-panner.refDistance = 1;
-panner.maxDistance = 10000;
-panner.rolloffFactor = 0.05;
-panner.coneInnerAngle = 360;
-panner.coneOuterAngle = 0;
-panner.coneOuterGain = 1;
-panner.setOrientation(0,0,1);
-
-var listener = context.listener;
-listener.setOrientation(0,0,-1,0,1,0);
+var panner;
 
 // set up listener and panner position information
 var WIDTH = window.innerWidth;
@@ -23,15 +8,10 @@ var xPos = Math.floor(WIDTH / 2);
 var yPos = Math.floor(HEIGHT / 2);
 var zPos = 300;
 
-// listener will always be in the same place for this demo
-listener.setPosition(xPos,yPos,300);
-
 // panner will move as the boombox graphic moves around on the screen
 function positionPanner(x, y, z) {
   panner.setPosition(x, y, z);
 }
-
-positionPanner(xPos, yPos, zPos);
 
 function center() {
   positionPanner(xPos, yPos, zPos);
@@ -45,20 +25,56 @@ function right() {
   positionPanner(xPos * 1.05, yPos, zPos);
 }
 
-window.positionPanner = positionPanner;
-
-window.center = center;
-
-window.left = left;
-
-window.right = right;
-
-panner.connect(context.destination);
-
-for (var i = 0; i < 10; i++) {
-  var osc = context.createOscillator();
-  osc.type = 'square';
-  osc.frequency.value = 40 + i * 0.1111; // value in hertz
-  osc.connect(panner);
-  osc.start();
+function bootstrapGlobals() {
+  window.positionPanner = positionPanner;
+  window.center = center;
+  window.left = left;
+  window.right = right;
 }
+
+function setupPanner(context) {
+  // panner taken from https://developer.mozilla.org/en-US/docs/Web/API/PannerNode
+  panner = context.createPanner();
+  panner.panningModel = 'HRTF';
+  panner.distanceModel = 'inverse';
+  panner.refDistance = 1;
+  panner.maxDistance = 10000;
+  panner.rolloffFactor = 0.05;
+  panner.coneInnerAngle = 360;
+  panner.coneOuterAngle = 0;
+  panner.coneOuterGain = 1;
+  panner.setOrientation(0,0,1);
+}
+
+function makeOscillators(context, panner) {
+  for (var i = 0; i < 10; i++) {
+    var osc = context.createOscillator();
+    osc.type = 'square';
+    osc.frequency.value = 40 + i * 0.1111; // value in hertz
+    osc.connect(panner);
+    osc.start();
+  }
+}
+
+function init() {
+  var context = new (window.AudioContext || window.webkitAudioContext)();
+  setupPanner(context);
+  var gain = context.createGain();
+  gainNode.gain.value = 0.6;
+  var listener = context.listener;
+  listener.setOrientation(0,0,-1,0,1,0);
+  // listener will always be in the same place for this demo
+  listener.setPosition(xPos,yPos,300);
+  positionPanner(xPos, yPos, zPos);
+  panner.connect(gain.connect(context.destination));
+  makeOscillators(context, panner);
+  
+  bootstrapGlobals();
+}
+
+
+module.exports = {
+  init: init,
+  positionPanner: positionPanner
+};
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-require('./audio');
+// var _ = require('lodash')
+var once = require('lodash/once');
+
+var audio = require('./audio');
 require('./orb');
+
+var startDiv = document.getElementById('start');
+var tux = document.getElementById('content');
+var orb = document.getElementById('draggable-element');
+
+function setupUI() {
+  tux.style.display = 'flex';
+  orb.style.display = 'block';
+  startDiv.style.display = 'none';
+}
+
+function init() {
+  setupUI();
+  audio.init();
+}
+
+startDiv.onclick = once(init);
